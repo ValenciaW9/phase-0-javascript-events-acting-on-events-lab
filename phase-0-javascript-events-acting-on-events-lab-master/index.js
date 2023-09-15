3c4ecd426b43e712a9949ab11abae0e74dd81a89
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-15 16:10:36 -0400

    phase-0-javascript-events-acting-on-events-lab/
    phase-0-javascript-events-acting-on-events-lab/

--- phase-0-javascript-events-acting-on-events-lab-master/index.js
+++ phase-0-javascript-events-acting-on-events-lab-master/index.js
@@ -1,35 +1,50 @@
-// Your code here
 // Import any necessary testing dependencies or modules
-const { expect } =  require ('chai');
+const { expect } = require('chai');
 
 // Import the code to be tested
 const index = require('../index');
 
+// Define the moveDodgerLeft function
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const currentPosition = parseInt(dodger.style.left);
+    if (currentPosition > 0) {
+        dodger.style.left = `${currentPosition - 1}px`;
+    }
+}
+
+// Define the moveDodgerRight function
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const currentPosition = parseInt(dodger.style.left);
+    if (currentPosition < 360) {
+        dodger.style.left = `${currentPosition + 1}px`;
+    }
+}
+
 // Describe the test suite
 describe('Index Tests', () => {
-  // Test case to check if dodger moves to the right on "ArrowRight" key press
-  it('should move the dodger to the right on "ArrowRight" key press', () => {
-    // Create a mock DOM environment
-    const { JSDOM } = require('jsdom');
-    const dom = new JSDOM('<!DOCTYPE html><div id="game"><div id="dodger"></div></div>', { runScripts: 'outside-only' });
-    global.window = dom.window;
-    global.document = dom.window.document;
+    // Test case to check if dodger moves to the right on "ArrowRight" key press
+    it('should move the dodger to the right on "ArrowRight" key press', () => {
+        // Create a mock DOM environment
+        const { JSDOM } = require('jsdom');
+        const dom = new JSDOM('<!DOCTYPE html><div id="game"><div id="dodger"></div></div>');
+        global.window = dom.window;
+        global.document = dom.window.document;
 
-    // Get the dodger element
-    const dodger = document.getElementById('dodger');
+        // Get the dodger element
+        const dodger = document.getElementById('dodger');
 
-    // Set the initial left position of the dodger
-    dodger.style.left = '0px';
+        // Set the initial left position of the dodger
+        dodger.style.left = '0px';
 
-    // Simulate the keydown event with "ArrowRight" key
-    const event = new dom.window.KeyboardEvent('keydown', { key: 'ArrowRight' });
-    document.dispatchEvent(event);
+        // Simulate the keydown event with "ArrowRight" key
+        const event = new dom.window.KeyboardEvent('keydown', { key: 'ArrowRight' });
+        document.dispatchEvent(event);
 
-    // Check if the dodger element's position has been updated correctly
-    const expectedPosition = '1px'; // We expect the dodger to move 1px to the right
-    const actualPosition = dodger.style.left;
-    expect(actualPosition).to.equal(expectedPosition);
-  });
+        // Check if the dodger element's position has been updated correctly
+        const expectedPosition = '1px'; // We expect the dodger to move 1px to the right
+        const actualPosition = dodger.style.left;
+        expect(actualPosition).to.equal(expectedPosition);
+    });
 });
-
-// Run the tests using a testing framework or test runner
